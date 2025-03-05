bab8f05e2ad09fd7361b055119093557b6a6ec80
deepakgla16 <deepaksharma972079@gmail.com>
2025-03-05 17:13:05 +0530

    Service Layer

--- src/main/java/com/example/GreetingApp/controller/GreetingController.java
+++ src/main/java/com/example/GreetingApp/controller/GreetingController.java
@@ -1,12 +1,8 @@
 package com.example.GreetingApp.controller;
 
 import com.example.GreetingApp.controller.Greeting;
-import com.example.GreetingApp.model.GreetingModel;
 import com.example.GreetingApp.services.GreetingService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/greeting")
@@ -38,80 +34,8 @@ public class GreetingController {
     }
 
     @GetMapping("/service")
+
     public Greeting getGreetingWithService() {
         return new Greeting(greetingService.getGreetingMessage());
     }
-
-    @GetMapping("/param")
-    public Greeting getGreetingfirstNameAndLastName(
-            @RequestParam(value = "firstName", required = false) String firstName,
-            @RequestParam(value = "lastName", required = false) String lastName) {
-
-        String message = greetingService.getGreetingMessageWithName(firstName, lastName);
-        return new Greeting(message);
-    }
-
-    @GetMapping("/greeting-save")
-    public  GreetingModel getGreetingDetails(
-            @RequestParam(required = false) String firstName,
-            @RequestParam(required = false) String lastName){
-        return greetingService.getGreetingMessageTosave(firstName,lastName);
-    }
-    @PostMapping("/greeting-save")
-    public GreetingModel saveGreeting(@RequestBody GreetingRequest request){
-        return greetingService.getGreetingMessageTosave(request.getFirstName(),request.getLastName());
-    }
-
-    public static class GreetingRequest{
-        private String firstName;
-        private String lastName;
-
-        public String getFirstName(){
-            return firstName;
-        }
-        public String getLastName(){
-            return lastName;
-        }
-
-        public void setFirstName(String firstName){
-            this.firstName=firstName;
-        }
-
-        public  void setLastName(String lastName){
-            this.lastName=lastName;
-        }
-    }
-
-    @GetMapping("/greeting-find/{id}")
-    public GreetingModel getGreetingById(@PathVariable Long id){
-        return  greetingService.getGreetingId(id);
-    }
-
-    @GetMapping("/greeting-all")
-    public List<GreetingModel> getAllGreetings(){
-        return greetingService.getAllGreetings();
-    }
-
-    @PutMapping("/greeting-update/{id}")
-    public ResponseEntity<GreetingModel> updatingGreeting(@PathVariable Long id, @RequestBody GreetingUpdateRequest request){
-        GreetingModel updatedGreeting= greetingService.updateGreeting(id,request.getMessage());
-        return ResponseEntity.ok(updatedGreeting);
-    }
-
-    public static class GreetingUpdateRequest{
-        private  String message;
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-    }
-    @DeleteMapping("/greeting-delete/{id}")
-    public ResponseEntity<String> deleteGreeting(@PathVariable Long id){
-        greetingService.deleteGreeting(id);
-        return  ResponseEntity.ok("Deleted successfully");
-    }
-
 }
