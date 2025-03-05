ec26560861c77027bb95b588ce35608ae7366984
deepakgla16 <deepaksharma972079@gmail.com>
2025-03-05 16:01:05 +0530

    update main

--- src/main/java/com/example/GreetingApp/repository/GreetingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.GreetingApp.repository;
-
-import com.example.GreetingApp.model.GreetingModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GreetingRepository extends JpaRepository<GreetingModel, Long> {
-}
